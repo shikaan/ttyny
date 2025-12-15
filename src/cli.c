46de8788550492820fffe63c0b3892661aa97662
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-12-15 13:55:52 +0100

    fix: catch missing file in path

--- src/cli.c
+++ src/cli.c
@@ -57,7 +57,7 @@ void cliPrintUsageAndExit(void) {
           "%s is a small-language-model-powered game engine to play text "
           "adventure games in your terminal.\n"
           "Usage:\n"
-          "  %s <path-to-story.json>\n"
+          "  %s [flags] <path-to-story.json>\n"
           "\n"
           "Flags:\n"
           "  -h, --help         show this help\n"
@@ -69,8 +69,8 @@ void cliPrintUsageAndExit(void) {
   exit(1);
 }
 
-static log_level_t parseLogLevel(const char *arg, int len) {
-  const int diff = (arg[len] == '=') ? len + 1 : len;
+static log_level_t parseLogLevel(const char *arg, unsigned long len) {
+  const unsigned long diff = (arg[len] == '=') ? len + 1 : len;
 
   if (!strcmp(arg + diff, "info")) {
     return LOG_LEVEL_INFO;
@@ -93,15 +93,12 @@ static log_level_t parseLogLevel(const char *arg, int len) {
 }
 
 void cliParseArgs(int argc, char **argv, cli_args_t *args) {
-  if (argc > 3) {
+  if (argc < 2 || argc > 3) {
     cliPrintUsageAndExit();
   }
 
-  args->log_level = LOG_LEVEL_ERROR;
-  args->story_path = argv[argc - 1];
-
-  for (int i = 0; i < argc - 1; i++) {
-    const char *arg = argv[i];
+  if (argc == 2) {
+    const char *arg = argv[argc - 1];
 
     if (!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
       cliPrintUsageAndExit();
@@ -112,12 +109,44 @@ void cliParseArgs(int argc, char **argv, cli_args_t *args) {
       exit(1);
     }
 
-    if (!strncmp(arg, "-l", 2)) {
-      args->log_level = parseLogLevel(arg, 2);
+    if (!strncmp(arg, "-l", 2) || !strncmp(arg, "--log", 5)) {
+      cliPrintError("missing story");
+      cliPrintUsageAndExit();
     }
 
-    if (!strncmp(arg, "--log", 5)) {
-      args->log_level = parseLogLevel(arg, 5);
+    if (arg[0] == '-') {
+      cliPrintError("unrecognized flag");
+      cliPrintUsageAndExit();
+    }
+
+    args->log_level = LOG_LEVEL_ERROR;
+    args->story_path = arg;
+    return;
+  }
+
+  if (argc == 3) {
+    args->log_level = LOG_LEVEL_ERROR;
+    args->story_path = argv[argc - 1];
+
+    for (int i = 1; i < argc - 1; i++) {
+      const char *arg = argv[i];
+
+      if (!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
+        cliPrintUsageAndExit();
+      }
+
+      if (!strcmp(arg, "-v") || !strcmp(arg, "--version")) {
+        fprintf(stderr, "%s - %s (%s)\n", NAME_NO_TTY, VERSION, SHA);
+        exit(1);
+      }
+
+      if (!strncmp(arg, "-l", 2) || !strncmp(arg, "--log", 5)) {
+        args->log_level = parseLogLevel(arg, strlen(arg));
+        continue;
+      }
+
+      // unrecognised parameter
+      cliPrintUsageAndExit();
     }
   }
 }
